ac82ebe187efb7a7e95a23addb0e2986a5083ece
Vasya Rovnov <vasyarovnov0802@gmail.com>
2022-02-16 21:34:59 +0400

    3ita

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
-import { Card } from "./Cards.js";
-import { FormValidator } from "./validate.js"
+import { Card } from "./Card.js";
+import { FormValidator } from "./FormValidator.js"
 import {initialCards, profileName, profileJob, editButton, popupEdit, closeForm, cardTemplateSelector, cardList,
   profileForm, nameInput, jobInput, addButton, popupAdd, 
   closeFormAdd, cardNameInput,cardLinkInput, formAdd, slider, closeSlider, overlayProfile, overlayPlace, overlayPhoto, enableValidator } from "./constants.js"
@@ -8,26 +8,28 @@ import { openPopup, closePopup } from "./utils.js";
 //валидация формы редактирования профиля
 const editFormValidator = new FormValidator(enableValidator, profileForm);
 editFormValidator.enableValidation();
+editFormValidator.resetValidForm();
 
 //валидация формы добавления новой карточки
 const addCardFormValidator = new FormValidator(enableValidator, formAdd);
 addCardFormValidator.enableValidation();
+addCardFormValidator.resetValidForm();
 
-const renderCard = (data) => {
+function createCard(data) {
   const card = new Card(data, cardTemplateSelector)
   const cardListItem = card.createCard()
-
-  cardList.prepend(cardListItem);
-};
+  return  cardListItem // возваращаете готовую карточку
+}
 
 initialCards.forEach((data) => {
-  renderCard(data, cardList)
+  cardList.prepend(createCard(data, cardList))
 })
 //ф-я открыть форму профиля
 function editProfile(){
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
   openPopup (popupEdit);
+  editFormValidator.resetValidForm();
 }
 //ф-я сохранения нового профиля
 function saveProfile(evt){
@@ -39,33 +41,29 @@ function saveProfile(evt){
 //ф-я сохранения карты
 function saveAddCard(evt){
   evt.preventDefault();
-  renderCard({ link: cardLinkInput.value, name:cardNameInput.value});
+  cardList.prepend(createCard({ link: cardLinkInput.value, name:cardNameInput.value}));
   formAdd.reset();
   closePopup (popupAdd);
  
 }
 function closePopupEdit(evt) {
   closePopup(popupEdit, evt)
-  editFormValidator.resetValidForm()
+  
   
 }
 function closePopupAdd(evt) {
 closePopup(popupAdd, evt)
-addCardFormValidator.resetValidForm()
 formAdd.reset()
 }
-function disableSubmitBtn(submitBtnElement){
-  submitBtnElement.disabled=true;
-  submitBtnElement.classList.add("popup__save_disabled");}
+
+ 
 //обработчики работы с профилем
 editButton.addEventListener ('click', editProfile);//добавить
 closeForm.addEventListener ('click', closePopupEdit);//закрыть
 profileForm.addEventListener('submit', saveProfile);//сохранить
 //обработчики работы с картами
 addButton.addEventListener ('click', function() {
-  openPopup(popupAdd);
-  const buttonAdd = popupAdd.querySelector(".popup__save");
-  disableSubmitBtn(buttonAdd);});//добавить
+  openPopup(popupAdd);addCardFormValidator.resetValidForm()});//добавить
 closeFormAdd.addEventListener ('click', closePopupAdd);//закрыть
 formAdd.addEventListener('submit', saveAddCard);//сохранить
 //для закрытия слайда вынесен из ф-и
